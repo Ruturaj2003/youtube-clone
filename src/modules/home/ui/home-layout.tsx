6890328c8635934c16f147e58c10eb688ab1ff2a
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-08-14 14:32:16 +0530

    Implement Navbar UI

--- src/modules/home/ui/home-layout.tsx
+++
@@ -1,7 +0,0 @@
-interface HomeLayoutProps {
-  children: React.ReactNode;
-}
-
-export const HomeLayout = ({ children }: HomeLayoutProps) => {
-  return <div className="bg-slate-700">{children}</div>;
-};
