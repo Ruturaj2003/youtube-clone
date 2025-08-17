4c23ef0db2a1320b4bb24b9ecf66141dee27ef6e
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-08-17 14:49:25 +0530

    Trpc half config

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,17 +1,20 @@
 import { z } from "zod";
-import { baseProcedure, createTRPCRouter } from "../init";
+import { createTRPCRouter, protectedProcedure } from "../init";
+
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
+  hello: protectedProcedure
     .input(
       z.object({
         text: z.string(),
       })
     )
     .query((opts) => {
+      console.log({ dbUser: opts.ctx.clerkUserId });
       return {
-        greeting: `hello ${opts.input.text}`,
+        greeting: `Hello ${opts.input.text}`,
       };
     }),
 });
-// export type definition of API
+
+// Export type definition of API
 export type AppRouter = typeof appRouter;
